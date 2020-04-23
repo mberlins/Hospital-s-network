27e320ce526d3e5bcd3485da2d51466bf49d4f82
mberlins <62808050+mberlins@users.noreply.github.com>
2020-04-23 22:26:57 +0200

    Add files via upload
    Specimen ma swoją kopię hospitals i field

--- src/sample/Specimen.java
+++ src/sample/Specimen.java
@@ -9,7 +9,6 @@ public class Specimen
     final int y_extent = 649;
     int field[][] = new int[649][689];
 
-    public int[] getHospitals() { return hospitals; }
 
     public int getGeneration()
     {
@@ -31,7 +30,35 @@ public class Specimen
         this.size = size;
     }
 
+    public int[] getHospitals()
+    {
+        return hospitals;
+    }
 
+    public void setHospitals(int[] hospitals)
+    {
+        this.hospitals = hospitals;
+    }
+
+    public int getX_extent()
+    {
+        return x_extent;
+    }
+
+    public int getY_extent()
+    {
+        return y_extent;
+    }
+
+    public int[][] getField()
+    {
+        return field;
+    }
+
+    public void setField(int[][] field)
+    {
+        this.field = field;
+    }
 
     /**
      * Konstruktor klasy Specimen
@@ -39,13 +66,15 @@ public class Specimen
      * @param map przechowuje informacje o pokryciu powierzchni kraju przez siatkę szpitali, powinien być przekazywany
      *            przez osobnika poprzedniej generacji
      */
-    public Specimen(int generation, Map map, int[] hospitals, int number, int size) // (poprzednik.getGeneration(), poprzednik.getCities(), poprzednik.getMap(), number dostaje od algorytmu)
+    public Specimen(int generation, Map map, int[] predecessors_hospitals, int number, int size) // (poprzednik.getGeneration(), poprzednik.getCities(), poprzednik.getMap(), number dostaje od algorytmu)
     {
         this.generation = generation;
         this.size = size-1;
-        this.hospitals = hospitals;
 
         System.arraycopy(map.getField(), 0, this.field, 0, 447161);
+
+        hospitals = new int[74];
+        System.arraycopy(predecessors_hospitals, 0, this.hospitals,0, 74);
         hospitals[number]=0;
 
     }
@@ -55,11 +84,9 @@ public class Specimen
         this.generation = generation;
         this.size = map.getSize();
 
-
-        hospitals = new int [75];
-        for(int i = 1; i <= 74; i++)
-            hospitals[i] = 1;
-
+        hospitals = new int [74];
+        for(int i = 0; i < 74; i++)
+            hospitals[i] = i+1;
     }
 
 
