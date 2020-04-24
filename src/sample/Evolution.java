74764b5fb4811eb6e8eb9ab600dfcc35f02fd32f
mberlins <62808050+mberlins@users.noreply.github.com>
2020-04-24 18:36:35 +0200

    Added comments.

--- src/sample/Evolution.java
+++ src/sample/Evolution.java
@@ -1,62 +1,120 @@
 package sample;
 
+/**
+ * Klasa evoulution odpowiada za realizację algorytmu ewolucyjnego
+ */
 public class Evolution
 {
+    /**
+     * Obiekt klasy Country
+     */
     private Country country;
 
+    /**
+     * Obiekt klasy Map
+     */
     private Map map;
 
+    /**
+     * Obiekt klasy Specimen
+     */
     private Specimen specimen_bis;
 
+    /**
+     * Drugi obiekt klasy Specimen
+     */
     private Specimen specimen_ter;
 
+    /**
+     * Indeks miasta, w którym usunięto szpital
+     */
     private int eliminated = 0;
 
+    /**
+     * Licznik przechowujący informację ile razy z rzędu algorytm wybrał pierwszego osobnika
+     */
     private int counter_bis = 0;
 
+    /**
+     * Licznik przechowujący informację ile razy z rzędu algorytm wybrał drugiego osobnika
+     */
     private int counter_ter = 0;
 
+    /**
+     * Indeks miasta, które zostało zastąpione
+     */
     private int replaced;
+
+    /**
+     * Indeks miasta, które zostało dodane w opracji zastąpienia
+     */
     private int added;
 
+    /**
+     * Licznik pokoleń
+     */
+    private int counter = 20;
+
+    /**
+     * @return licznik pokoleń
+     */
     public int getCounter()
     {
         return counter;
     }
 
-    private int counter = 20;
-
-
+    /**
+     * @return obiekt klasy Country
+     */
     public Country getCountry()
     {
         return country;
     }
 
+    /**
+     * @param country przekazuje do ustawienia obiekt klasy Country
+     */
     public void setCountry(Country country)
     {
         this.country = country;
     }
 
+    /**
+     * @return obiekt klasy Map
+     */
     public Map getMap()
     {
         return map;
     }
 
+    /**
+     * @param map przekazuje do ustawienia obiekt klasy Map
+     */
     public void setMap(Map map)
     {
         this.map = map;
     }
 
+    /**
+     * @return pierwszego z obiektów klasy Specimen
+     */
     public Specimen getSpecimen_bis()
     {
         return specimen_bis;
     }
 
+    /**
+     * @param specimen_bis ustawia obiekt klasy Specimen na zadany
+     */
     public void setSpecimen_bis(Specimen specimen_bis)
     {
         this.specimen_bis = specimen_bis;
     }
 
+    /**
+     * Konstruktor tworzący obiekt klasy Country przechowujący dane miast i granic, mapę szpitali oraz granic, a także inicjalizujący
+     * pierwszego osobnika klasy Specimen
+     */
     public Evolution()
     {
         country = new Country();
@@ -65,6 +123,12 @@ public class Evolution
         specimen_ter = new Specimen(0, map);
     }
 
+    /**
+     * Funkcja zarządzająca procesem tworzenia nowych osobników (2 rodzaje mutacji) oraz wybierająca z populacji dwuelementowej
+     * osobniika lepiej przystosowanego, który przekaże swoje geny. Mutacja może usunąć losowe miasto ze szpitalem z mapy
+     * lub losowo usunąć jedno miasto i zastąpić je innym.
+     * @return ostatniego osobnika (obiekt klasy Specimen)
+     */
     public Specimen evolution_control()
     {
         int check_ter = 1000;
@@ -132,6 +196,11 @@ public class Evolution
 
     }
 
+    /**
+     * Funkcja tworząca nowego osobnika na podstawie starego - usuwa losowe miasto.
+     * @param specimen jest osobnikiem przekazującym geny
+     * @param id przekazuje informację, którego osobnika należy zastąpić
+     */
     public void replace_specimen(Specimen specimen, int id)
     {
         int check = 0;
@@ -166,6 +235,11 @@ public class Evolution
 
     }
 
+    /**
+     * Funkcja tworząca nowego osobnika na podstawie starego - usuwa losowe miasto i dodaje inne losowo wybrane miasto(wcześniej usunięte).
+     * @param specimen jest osobnikiem przekazującym geny
+     * @param id przekazuje informację, którego osobnika należy zastąpić
+     */
     public void mutation(Specimen specimen, int id)
     {
         int check = 0;
